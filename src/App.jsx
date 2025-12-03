65bdce43f0a4078159a2a9790c53abf2043e5c1e
fruttolo <s.contino97@gmail.com>
2025-12-03 19:03:44 +0100

    refactor: update App component to generate and display DID, modify package.json dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
-import React from 'react'
+import { util } from "@cef-ebsi/ebsi-did-resolver";
+import React, { useEffect, useState } from 'react'
 
 export default function App() {
+
+  const [did, setDid] = useState("");
+
+  function generateDid() {
+    // create an array of 16 random bytes with react
+    const subjectIdentifierBytes = new Uint8Array(16);
+    window.crypto.getRandomValues(subjectIdentifierBytes);
+
+    const did = util.createDid(subjectIdentifierBytes);
+    setDid(did);
+  }
+
   return (
     <div className="app-root">
       <header>
-        <h1>Capacitor + React (JSX) — Hello World 👋</h1>
+        <h1>DID Ebsi</h1>
       </header>
 
       <main>
         <p>
-          This is a minimal React app scaffolded for Capacitor. Build it and
-          connect with Capacitor to make native mobile apps.
+          {`Your generated DID is: ${did}`}
         </p>
 
         <div className="card">
           <button
             onClick={() => {
-              alert('Hello from Capacitor + React!')
+              generateDid();
             }}
           >
-            Say Hello
+            Generate New DID
           </button>
         </div>
       </main>
-
-      <footer>
-        <small>Built with Vite + React + Capacitor</small>
-      </footer>
     </div>
   )
 }
